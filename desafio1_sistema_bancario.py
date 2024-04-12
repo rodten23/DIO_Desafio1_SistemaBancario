ad71129113a671f7bb34c3c2ec1e4e786ce20c19
rodten23 <rodten23@gmail.com>
2024-04-12 20:02:25 -0300

    :sparkles: feat: Início do desenvolvimento da funcionalidade de depósito.

--- desafio1_sistema_bancario.py
+++ desafio1_sistema_bancario.py
@@ -22,7 +22,10 @@ while True:
     opcao = input(menu)
 
     if opcao == 'd':
-        print('Fazer Depósito')
+        print('Fazer Depósito (aceitamos apenas notas):\n')
+        valor_deposito = input('Por favor, informe o valor que será depositado? => ')
+        if valor_deposito < 2:
+            print('Infelizmente não aceitamos moedas.\n Favor, informe novamente valor a ser depositado.\n')
 
     elif opcao == 's':
         print('Efetuar Saque')
