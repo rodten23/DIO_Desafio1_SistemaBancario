c35bbed24e92599ced32f93ecd94c36c0864997e
rodten23 <rodten23@gmail.com>
2024-04-12 19:33:26 -0300

    :sparkles: feat: Inclusão da estrutura básica While.

--- desafio1_sistema_bancario.py
+++ desafio1_sistema_bancario.py
@@ -10,3 +10,23 @@ LIMITE_VALOR_SAQUE_DIARIO = 500
 extrato = ''
 saques_feitos = 0
 LIMITE_SAQUES = 3
+
+while True:
+
+    opcao = input(menu)
+
+    if opcao == 'd':
+        print('Fazer Depósito')
+
+    elif opcao == 's':
+        print('Efetuar Saque')
+
+    elif opcao == 'e':
+        print('Exibir Extrato')
+
+    elif opcao == 'x':
+        print('Saindo do Sistema...')
+        break
+
+    else:
+        print('Operação inválida! Por favor, selecione novamente a operação desejada.')
